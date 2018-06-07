1467e2912e047d83a0da67fac1e6477895201552
Garrett Clary <claryg0603@protonmail.com>
2018-06-07 14:20:21 -0700

    updated requirements.txt and made minor edits to variable names

--- passwdgen.py
+++ passwdgen.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3
-#
-# A simple password generator using Python's "secrets".
-#
-
 
 import string
 import secrets
@@ -12,18 +8,18 @@ import sys
 
 def usage():
     file_name = os.path.basename(sys.argv[0])
-    print('usage: ' + file_name + ' <password_length>')
+    print("usage: " + file_name + " <password_length>")
     sys.exit()
     
-def gen_passwd(pwd_length):
-    pwd_length = int(sys.argv[1])
+def generate_password(password_length):
+    password_length = int(sys.argv[1])
     usable = string.ascii_letters + string.digits + string.punctuation
-    pwd = ''
+    password = ''
     
-    for i in range(pwd_length):
-        pwd += secrets.choice(usable)
+    for i in range(password_length):
+        password += secrets.choice(usable)
     
-    print(pwd)
+    print(password)
 
 
 def main():
@@ -32,12 +28,12 @@ def main():
     else:
         try:
             int(sys.argv[1])
-            if int(sys.argv[1]) < 8:
-                print('You can do better than that...')
+            if int(sys.argv[1]) < 10:
+                print("Password length must be at least 10 characters")
                 sys.exit()
-            gen_passwd(sys.argv[1])
+            generate_password(sys.argv[1])
         except ValueError:
-            print('Length of Password must be a whole number (8, 12, 15, etc)')
+            print("Length of Password must be a whole number (8, 12, 15... )")
             sys.exit()
 
 if __name__ == '__main__':
